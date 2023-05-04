9aab9d11e9ef2d25d05222720a67758916e15a62
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-05-04 21:41:29 +0900

    initS

---
+++ src/main/java/com/example/awsprac/AwsPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPracApplication.class, args);
+	}
+
+}
